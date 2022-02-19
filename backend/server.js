6a2e939e78cf267cde3e208c305b64a61820273a
dev-naresh <85550007+dev-naresh@users.noreply.github.com>
2022-02-19 16:56:12 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,15 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+//Serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}else{
+    app.get('/', (req, res) => res.send('Please set to  production mode'))
+}
+
 app.use(errorHandler)
 
 app.listen (port, () => console.log(`Server is started at ${port}`))
